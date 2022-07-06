9d6e7a0721c9cc01882278c6d1ef5c3136653426
Kr1s4an <103644555+Kr1s4an@users.noreply.github.com>
2022-07-06 17:06:21 +0300

    interface

---
+++ src/Model/AuthorshipDetectorImpl.java
@@ -0,0 +1,10 @@
+package Model;
+
+import java.io.InputStream;
+
+public class AuthorshipDetectorImpl implements AuthorshipDetector {
+  public AuthorshipDetectorImpl(InputStream signaturesDataset, double[] weights){
+
+  }
+
+}
